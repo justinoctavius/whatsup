f4bb54f0525083a3ebf591d778781de594977574
octavio <justinoctavio2001@gmail.com>
2020-07-26 09:23:56 -0400

    'fixing validation login

--- controllers/login.js
+++ controllers/login.js
@@ -5,11 +5,13 @@ const ctrl = {}
 ctrl.find = async (req, res) => {
     const { username, password } = req.body;
     const user = await User.findOne({username: username});
-    const allow = crypt.compare(user.password,password);
-    if(allow) {
-        res.json(user);
-    }else{
-        res.json(false)
+    if(user){
+        const allow = crypt.compare(user.password,password);
+        if(allow) {
+            res.json(user);
+        }else{
+            res.json(false)
+        }
     }
 }
 
